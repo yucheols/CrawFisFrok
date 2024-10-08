f14904421bf15397f85e57cb59a17cb5ec251ede
Yucheol Shin <brongersmai2@gmail.com>
2024-10-08 18:02:24 -0400

    commit codes and files

--- codes/crawfisfrok_ENMs.R
+++ codes/crawfisfrok_ENMs.R
@@ -24,7 +24,7 @@
 ##                                                                                                          ##
 ##                                                                                     ##   ##              ## 
 ##                                                                                     ## ##                ##
-##                                                                                     ###                  ##   
+##                                                                                     ##                   ##   
 ##                                                                                     ###                  ##
 ##                                                                                     ## ##                ## 
 ##                                                                                     ##   ##              ##
@@ -36,12 +36,35 @@
 
 ###########    CrawFisFrok ENM == We go NOW
 
-# clear working env
+# kindly clear the working environment
 rm(list = ls(all.names = T))
 gc()
 
-# load packages
+# load packages we do {CLAP}
+library(ENMwrap)
+library(raster)
+library(sf)
+library(dplyr)
 library(megaSDM)
 
 
-#####  part 1 ::: get occurrence points
+#####  part 1 ::: get environmental data  ----------
+
+# define clipping extent
+ext <- c(-105.018,-84.675,28.311,44.003)
+
+# WorldClim 5km raster
+clim <- raster::stack(list.files(path = 'E:/env layers/worldclim', pattern = '.tif$', full.names = T))
+clim <- raster::crop(clim, ext)
+
+plot(clim[[1]])
+
+
+#####  part 2 ::: get occurrence points  ----------
+
+# collect occurrence points 
+OccurrenceCollection(spplist = c('Lithobates areolatus',
+                                 'Lithobates areolatus areolatus',
+                                 'Lithobates areolatus circulosus'),
+                     output = 'occs',
+                     trainingarea = extent(clim[[1]]))
