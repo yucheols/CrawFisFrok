037e6c6bb8e8a4e74c8cdc37cad622f7dd7ab014
Yucheol Shin <brongersmai2@gmail.com>
2024-10-08 02:59:11 -0400

    initial commit

---
+++ codes/crawfisfrok_ENMs.R
@@ -0,0 +1,11 @@
+###########    CrawFisFrok ENM == We go NOW
+
+# clear working env
+rm(list = ls(all.names = T))
+gc()
+
+# load packages
+library(megaSDM)
+
+
+#####  part 1 ::: get occurrence points
