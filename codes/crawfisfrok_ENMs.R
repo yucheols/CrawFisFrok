708323790feebb103da47a7e8e49406b79295611
Yucheol Shin <brongersmai2@gmail.com>
2024-10-12 16:04:03 -0400

    commit codes and files

--- codes/crawfisfrok_ENMs.R
+++ codes/crawfisfrok_ENMs.R
@@ -55,7 +55,7 @@ library(megaSDM)
 library(raster)
 library(sf)
 library(dplyr)
-
+library(rasterVis)
 
 #####  part 1 ::: get environmental data  ----------
 
@@ -149,8 +149,9 @@ print(cfis_res)
 write.csv(cfis_res, 'outputs/models/crawfisfrok_tuning_results.csv')
 
 # find optimal model
-cfis_opt <- cfis_res %>% dplyr::filter(or.10p.avg == min(or.10p.avg)) %>%
-  dplyr::filter(auc.diff.avg == min(auc.diff.avg)) %>% 
+cfis_opt <- cfis_res %>%
+  dplyr::filter(or.10p.avg == min(or.10p.avg)) %>%
+  dplyr::filter(auc.diff.avg == min(auc.diff.avg)) %>%
   dplyr::filter(auc.val.avg == max(auc.val.avg))
 
 # check variable contribution
@@ -164,3 +165,65 @@ cfis_opt_pred <- eval.predictions(cfis_mod)[[cfis_opt$tune.args]]
 plot(cfis_opt_pred)
 
 writeRaster(cfis_opt_pred, 'outputs/preds/crawfisfrok_optmodel_pred.tif')
+
+
+#####  part 6 ::: response curves ----------
+
+# resp curve data
+cfis_resp_data <- ENMwrap::resp_data_pull(sp.name = 'crawfisfrok', model = eval.models(cfis_mod)[[cfis_opt$tune.args]], names.var = names(clim.subs))
+head(cfis_resp_data)
+
+# plot response
+ENMwrap::plot_response(resp.data = cfis_resp_data)
+
+
+#####  part 7 ::: hindcasting ----------
+
+# import LGM layers
+lgm <- raster::stack(list.files(path = 'E:/env layers/chelsa_LGM_v1_2B_r30s/30sec', pattern = '.tif$', full.names = T))
+lgm <- raster::crop(lgm, ext)
+plot(lgm[[1]])
+
+names(lgm) = gsub('_', '', names(lgm))
+
+# subset
+lgm <- raster::stack(subset(lgm, names(clim.subs)))
+print(lgm)
+
+# check climate units
+ENMwrap::unit_check(ref.env = clim.subs, proj.env = lgm, n = 10000)
+
+# it o'co proceed to projection
+cfis_lgm <- dismo::predict(object = eval.models(cfis_mod)[[cfis_opt$tune.args]], x = lgm, progress = 'text')
+plot(cfis_lgm)
+
+
+#####  part 8 ::: plot predictions ----------
+
+# stack them
+preds <- raster::stack(cfis_opt_pred, cfis_lgm)
+names(preds) = c('Current', 'LGM')
+
+# plot
+gplot(preds) + 
+  geom_tile(aes(fill = value)) + 
+  coord_equal() + 
+  facet_wrap(~ variable, ncol = 2, nrow = 1) + 
+  scale_fill_gradientn(colors = rev(as.vector(pals::ocean.thermal(1000))), 
+                       na.value = NA, 
+                       name = "Suitability", 
+                       breaks = c(0.1, 0.9), 
+                       labels = c(paste0("Low: ", 0.1), paste0("High: ", 0.9))) + 
+  xlab("Longitude") + ylab("Latitude") + 
+  theme_bw() + 
+  theme(strip.text = element_text(size = 14), 
+        legend.title = element_text(size = 14, face = "bold", margin = margin(b = 10)), 
+        legend.text = element_text(size = 12), 
+        axis.title = element_text(size = 14, face = "bold"), 
+        axis.title.x = element_text(margin = margin(t = 15)), 
+        axis.title.y = element_text(margin = margin(r = 15)), 
+        axis.text = element_text(size = 12))
+
+# export
+ggsave('outputs/plots/preds_output.png', width = 20, height = 8, dpi = 600, units = 'cm')
+
